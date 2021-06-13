31de6d15960b586be476e0b0a3e79cfc1f2236c8
Parisa Hezarosi <79167044+PH1030@users.noreply.github.com>
2021-06-13 22:26:25 +0430

    Update Func2.py

--- ex3/Func2.py
+++ ex3/Func2.py
@@ -1,8 +1,10 @@
-a = '546'
-res = 0
-value = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-
-for digit in a:
-    res = res * 10 + value[digit]
-
-print(res)
+def func2 (a) :
+    res = 0
+    value = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    for i in a :
+        if i!= '.' :
+            res = res * 10 + value[i]
+            b=a.index('.')
+            c=res/10**b
+    return c
+print (func2 (a))
